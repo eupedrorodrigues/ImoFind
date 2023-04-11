5a47278e6df9c03e6d97e6eed4dbe019aa4e7e56
Júlio César Amorim <amorim.jc@gmail.com>
2023-04-11 13:28:47 -0300

    criação da view dreamsVilleHouse

--- src/screens/DreamsVilleHouse/dreamsVilleHouseSTYLE.js
+++ src/screens/DreamsVilleHouse/dreamsVilleHouseSTYLE.js
@@ -22,9 +22,10 @@ export const styles = StyleSheet.create({
     borderRadius: 50,
     alignItems: "center",
     justifyContent: "center",
-    marginLeft: 10,
     position: 'absolute',
     zIndex: 100,
+    left: 50,
+    top: 20
   },
 
   favoriteIcon: {
@@ -34,24 +35,81 @@ export const styles = StyleSheet.create({
     borderRadius: 50,
     alignItems: "center",
     justifyContent: "center",
-    position: 'absolute'
+    position: 'absolute',
+    right: 50,
+    top: 20
   },
 
   upIMG: {
-    width: '80%',
+    width: 335,
     borderRadius: 20,
+  },
+
+  // imgDescriptionName: {
+  //   top: 211,
+  //   position: 'absolute'
+  // },
+
+  // imgDescriptionBenefits: {
+  //   flexDirection: 'row',
+  //   position: 'absolute',
+  //   // zIndex: 100
+  // },
+
+  descriptionContainer:{
+    width: '100%',
+    top: 10,
+    marginTop: 10,
+    paddingHorizontal: 20,
     
   },
 
-  imgDescriptionName: {
-    top: 211,
-    position: 'absolute'
+  proprietarioBox: {
+    width: '100%',
+    marginTop: 30,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'gray'
   },
 
-  imgDescriptionBenefits: {
-    flexDirection: 'row',
-    position: 'absolute',
-    zIndex: 100
-  }
+  proprietarioText: {
+    marginLeft: 15,
+    backgroundColor: 'orange'
+  },
 
+  proprietarioIcon: {
+    width: 100,
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: 'blue',
+    justifyContent: 'space-around',
+    marginLeft: 100
+  },
+
+  galeriaTitle: {
+    fontSize: 16, 
+    fontWeight: 'bold', 
+    paddingHorizontal: 20,
+    marginTop: 30,
+    marginBottom: 20
+  },
+
+  scrollView: {
+    backgroundColor: 'gray',
+
+  },
+
+  scrolledImg: {
+    marginLeft: 20,
+    // width: 222,
+    // height: 255,
+    justifyContent: 'center',
+  },
+
+  IMG: {
+    borderRadius: 5,
+    width: 72,
+    height: 72
+  }
 })
