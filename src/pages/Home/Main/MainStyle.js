9053562926afb209b4afd00d23339c89b96047ef
eupedrorodrigues <pedrorsrodrigues1203@gmail.com>
2023-05-25 19:49:30 -0300

    Alteracao Home

---
+++ src/pages/Home/Main/MainStyle.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    Contain:{
+        width: '110%',
+        height: 390,
+    },
+    ScrollHori:{
+        marginVertical: 10,
+        padding: 10,
+        paddingLeft: 30
+    },
+    HouseImg:{
+        width: 220,
+        height: 272,
+        resizeMode: 'cover',
+        borderRadius: 10
+    },
+    LinearGradient:{
+        height: 272,
+        borderRadius: 10, 
+    },
+    location:{
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        width: 60,
+        height: 25,
+        borderRadius: 20,
+        marginTop: 15,
+        marginLeft: 145,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    boxImage:{
+        marginHorizontal: 15
+    },
+    boxMainText:{
+        justifyContent:'space-between',
+        flexDirection:'row',
+        width: '77%',
+        marginTop: 30, 
+        marginLeft: 48,
+    },
+    boxMainTextIn:{
+        justifyContent:'space-between',
+        flexDirection:'row',
+        width: '77%', 
+        marginLeft: 48,
+    },
+    
+   
+
+})
